1a69a6aed08bb433900f885e50ccc426fcb7fdef
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-04 18:26:06 -0500

    feat: agregar configuración de AWS y recursos de Cognito, incluyendo issuer URI y variable de región

--- iac/environments/dev/outputs.tf
+++ iac/environments/dev/outputs.tf
@@ -7,3 +7,7 @@ output "dev_cognito_user_pool_client_id" {
   value = module.cognito.user_pool_client_id
   description = "ID del cliente para el frontend"
 }
+
+output "dev_cognito_issuer_uri" {
+  value = "https://cognito-idp.${var.aws_region}.amazonaws.com/${module.cognito.user_pool_id}"
+}
