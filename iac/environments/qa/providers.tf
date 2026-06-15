b35add1ed28aa8bd4bd29853c110e1873390a8fd
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-15 15:02:07 -0500

    feat: configurar backend remoto en los 3 entornos (dev, qa, prod)

--- iac/environments/qa/providers.tf
+++ iac/environments/qa/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "tesla-bootstrap-terraform-state-050608055215"
+    key    = "qa/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tesla-bootstrap-terraform-locks"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
