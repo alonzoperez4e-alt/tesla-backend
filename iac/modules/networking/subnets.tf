5baa0f37b6765b83057dcaf3d0aebf9a57185ca5
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-16 16:55:42 -0500

    feat: definir las subnets que iran dentro de la vpc

---
+++ iac/modules/networking/subnets.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.36.0"
+    }
+  }
+}
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets_cidr)
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = local.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets_cidr)
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+  availability_zone = local.azs[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnets_cidr)
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnets_cidr[count.index]
+  availability_zone = local.azs[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-database-subnet-${count.index}"
+  }
+}
