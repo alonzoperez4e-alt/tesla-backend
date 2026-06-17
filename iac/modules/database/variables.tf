690e7cb9eebed0fd6b655a4c86e38a894dc63033
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-17 13:27:24 -0500

    feat: declarar las variables a usar para el modulo de db

---
+++ iac/modules/database/variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {
+  description = "Nombre del proyecto"
+  type = string
+}
+
+variable "environment" {
+  description = "Entorno de despliegue (dev, qa, prod)"
+  type = string
+}
+
+variable "database_subnet_group_name" {
+  description = "Nombre del grupo de subredes para la base de datos"
+  type = string
+}
+
+variable "database_subnets" {
+  description = "Lista de subredes para la base de datos"
+  type = list(string)
+}
+
+variable "database_sg_id" {
+  description = "ID del grupo de seguridad para la base de datos"
+  type = string
+}
+
+variable "redis_sg_id" {
+  description = "ID del grupo de seguridad para Redis"
+  type = string
+}
+
+variable "db_username" {
+  description = "Nombre de usuario para la base de datos"
+  type = string
+}
+
+variable "db_password" {
+  description = "Contraseña para la base de datos"
+  type      = string
+  sensitive = true
+}
