eea092451ba78a7a25c113070bf80017671ab2ce
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-17 13:32:58 -0500

    feat: definir los outputs del modulo de bd

---
+++ iac/modules/database/outputs.tf
@@ -0,0 +1,9 @@
+output "postgres_endpoint" {
+  description = "Endpoint de conexión para PostgreSQL"
+  value       = aws_db_instance.postgres.endpoint
+}
+
+output "redis_endpoint" {
+  description = "Endpoint de conexión para Redis"
+  value       = aws_elasticache_cluster.redis.cache_nodes[0].address
+}
