871a0490214bac3072feb0260ceed8b256692b78
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-17 19:09:22 -0500

    fix: cambiar version de postgre

--- iac/modules/database/bd.tf
+++ iac/modules/database/bd.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "postgres" {
   identifier             = "${var.project_name}-${var.environment}-db"
   engine                 = "postgres"
-  engine_version         = "15.4"
+  engine_version         = "17"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
 
