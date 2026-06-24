212aabb142e9672bdeec60bc09288222ebe80bb8
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-24 11:40:12 -0500

    feat: indicarle al alb que solo acepte conexiones que provengan de las ips oficiales de cloudfront

--- iac/modules/security/alb.tf
+++ iac/modules/security/alb.tf
@@ -1,20 +1,20 @@
 resource "aws_security_group" "alb" {
   name = "${var.project_name}-${var.environment}-alb-sg"
-  description = "Permite trafico HTTP/HTTPS desde internet al ALB"
+  description = "Permite trafico HTTP/HTTPS solo desde CloudFront"
   vpc_id = var.vpc_id
 
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [data.aws_ec2_managed_prefix_list.cloudfront.id]
   }
 
   ingress {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [data.aws_ec2_managed_prefix_list.cloudfront.id]
   }
 
   egress {
