cfad985c081e1e2dffe562a2b8618435c98e2742
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-24 11:51:48 -0500

    feat: hacer que cloudfront inyecte una contraseña secreta en cada petición

--- iac/modules/edge/variables.tf
+++ iac/modules/edge/variables.tf
@@ -12,3 +12,8 @@ variable "alb_dns_name" {
   description = "El DNS publico del Load Balancer para conectarlo a CloudFront"
   type = string
 }
+
+variable "alb_secret_token" {
+  description = "Token secreto para validar la conexion entre CloudFront y el ALB"
+  type = string
+}
