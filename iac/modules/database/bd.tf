12a5e7dcc5051696bd5068177547ab3e61e111bd
Junior David Infantes Rondo <juniordavidinfantesrondo15@gmail.com>
2026-06-26 15:26:07 -0500

    fix: CKV_AWS_16

--- iac/modules/database/bd.tf
+++ iac/modules/database/bd.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "postgres" {
 
   skip_final_snapshot    = var.environment != "prod" ? true : false
   publicly_accessible    = false
+  storage_encrypted      = true
 
   deletion_protection    = var.environment == "prod" ? true : false
   
