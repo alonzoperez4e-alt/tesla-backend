8d61a6a87ce773cfc59f6d33cf77ae6c162fee44
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-26 16:52:28 -0500

    fix: CKV_AWS_163

--- iac/modules/compute/ecr.tf
+++ iac/modules/compute/ecr.tf
@@ -2,4 +2,8 @@ resource "aws_ecr_repository" "backend" {
   name = "${var.project_name}-${var.environment}-backend"
   image_tag_mutability = "MUTABLE"
   force_delete = var.environment != "prod" ? true : false
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
