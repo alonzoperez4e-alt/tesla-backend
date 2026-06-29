a604517b61cb129f8c394058404df6c6d9e7d245
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-06-28 19:29:18 -0500

    fix: CKV_AWS_174 + CKV2_AWS_42

--- iac/modules/edge/cloudfront.tf
+++ iac/modules/edge/cloudfront.tf
@@ -85,7 +85,8 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true # para pruebas
+    # checkov:skip=CKV2_AWS_42: Se utiliza el certificado por defecto ya que no se cuenta con un dominio propio.
+    cloudfront_default_certificate = true
   }
 
   tags = {
