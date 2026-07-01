285dbeef2847579c800684290fe795d746fd8e89
Alonzo Pérez Cristhian <alonzoperez4e@gmail.com>
2026-07-01 00:09:41 -0500

    feat: Integrar el alojamiento edge ​​hosting con S3 OAC, enrutamiento y políticas de solicitud de API

--- iac/modules/edge/cloudfront.tf
+++ iac/modules/edge/cloudfront.tf
@@ -1,31 +1,70 @@
 resource "aws_cloudfront_origin_access_control" "spa_oac" {
-  name                              = "${var.project_name}-${var.environment}-oac"
-  description                       = "Origin Access Control el bucket del Frontend"
+  name                              = "${var.project_name}-${var.environment}-spa-oac"
+  description                       = "Origin Access Control para el bucket del Frontend"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
 
+resource "aws_cloudfront_origin_access_control" "images_oac" {
+  name                              = "${var.project_name}-${var.environment}-images-oac"
+  description                       = "Origin Access Control para el bucket de Imagenes"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_origin_request_policy" "api_policy" {
+  name = "${var.project_name}-${var.environment}-api-policy"
+
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Authorization", "Origin", "Access-Control-Request-Headers", "Access-Control-Request-Method"]
+    }
+  }
+
+  cookies_config {
+    cookie_behavior = "all"
+  }
+
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_s3_bucket" "cf_logs" {
+  bucket        = "${var.project_name}-cf-logs-${var.environment}"
+  force_destroy = var.environment != "prod" ? true : false
+}
+
 resource "aws_cloudfront_distribution" "cdn" {
-  enabled = true
-  is_ipv6_enabled = true
+  enabled             = true
+  is_ipv6_enabled     = true
   default_root_object = "index.html"
-  price_class = "PriceClass_100"
+  price_class         = "PriceClass_100"
+
+  logging_config {
+    bucket          = aws_s3_bucket.cf_logs.bucket_domain_name
+    prefix          = "cf-access-logs/"
+    include_cookies = false
+  }
 
   origin {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id   = "S3-Frontend"
+    domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id                = "S3-Frontend"
     origin_access_control_id = aws_cloudfront_origin_access_control.spa_oac.id
   }
 
   origin {
     domain_name = var.alb_dns_name
     origin_id   = "ALB-Backend"
+
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "http-only" # para pruebas
-      origin_ssl_protocols = ["TLSv1.2"]
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
 
     custom_header {
@@ -35,62 +74,63 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   origin {
-    domain_name = aws_s3_bucket.images.bucket_regional_domain_name
-    origin_id   = "S3-Images"
-    origin_access_control_id = aws_cloudfront_origin_access_control.spa_oac.id
+    domain_name              = aws_s3_bucket.images.bucket_regional_domain_name
+    origin_id                = "S3-Images"
+    origin_access_control_id = aws_cloudfront_origin_access_control.images_oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "S3-Frontend"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
   ordered_cache_behavior {
-    path_pattern           = "/storage/*"
+    path_pattern           = "/api/*"
+    target_origin_id       = "ALB-Backend"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.api_policy.id
+  }
+
+  ordered_cache_behavior {
+    path_pattern           = "/ws-chat/*"
+    target_origin_id       = "ALB-Backend"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = false
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d5"
+  }
+
+  ordered_cache_behavior {
+    path_pattern           = "/images/*"
     target_origin_id       = "S3-Images"
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
-    cache_policy_id        = data.aws_cloudfront_cache_policy.caching_optimized.id
-  }
-
-  default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id       = "S3-Frontend"
-    viewer_protocol_policy = "redirect-to-https"
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
-  }
-
-  ordered_cache_behavior {
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods = ["GET", "HEAD"]
-    path_pattern           = "/api/*"
-    target_origin_id       = "ALB-Backend"
-    viewer_protocol_policy = "https-only"
-
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
-  }
-
-  ordered_cache_behavior {
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods = ["GET", "HEAD"]
-    path_pattern           = "/ws-chat/*"
-    target_origin_id       = "ALB-Backend"
-    viewer_protocol_policy = "https-only"
-
-    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
+    compress               = true
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
   custom_error_response {
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
-    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
   }
 
   custom_error_response {
-    error_code = 403
-    response_code = 200
-    response_page_path = "/index.html"
-    error_caching_min_ttl = 10
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
   }
 
   restrictions {
@@ -100,10 +140,11 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true # para pruebas
+    cloudfront_default_certificate = true
   }
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-cdn"
+    Name        = "${var.project_name}-${var.environment}-cdn"
+    Environment = var.environment
   }
 }
--- iac/modules/edge/s3.tf
+++ iac/modules/edge/s3.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "frontend" {
   bucket = "${var.project_name}-${var.environment}-frontend"
   force_destroy = var.environment != "prod" ? true : false
+
+  tags = {
+    Name        = "${var.project_name}-frontend-bucket"
+    Environment = var.environment
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "frontend" {
@@ -9,6 +14,45 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "frontend" {
+  bucket     = aws_s3_bucket.frontend.id
+  depends_on = [aws_s3_bucket_versioning.frontend]
+  rule {
+    id     = "cleanup-old-versions"
+    status = "Enabled"
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "frontend-policy" {
@@ -29,7 +73,6 @@ resource "aws_s3_bucket_policy" "frontend-policy" {
             "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
           }
         }
-
       }
     ]
   })
