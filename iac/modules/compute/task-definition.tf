d0f4b8a84dea5774f0b310db543ea3744b78a37d
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-07-02 13:59:02 -0500

    feat: modificar el punto de montaje en Terraform

--- iac/modules/compute/task-definition.tf
+++ iac/modules/compute/task-definition.tf
@@ -18,7 +18,7 @@ resource "aws_ecs_task_definition" "api" {
       mountPoints = [
         {
           sourceVolume  = "tomcat-tmp"
-          containerPath = "/tmp"
+          containerPath = "/tmp/tomcat"
           readOnly      = false
         }
       ]
