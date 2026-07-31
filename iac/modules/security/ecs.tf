ec803f47df88606844ffc0bb2902571309f28710
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-07-31 18:39:08 -0500

    chore: eliminar sg de redis y mq, y modificar el origen de las peticiones a aceptar

--- iac/modules/security/ecs.tf
+++ iac/modules/security/ecs.tf
@@ -14,7 +14,7 @@ resource "aws_security_group_rule" "ecs_ingress_alb" {
   to_port                  = 8080
   protocol                 = "tcp"
   security_group_id        = aws_security_group.ecs_tasks.id
-  source_security_group_id = aws_security_group.alb.id
+  source_security_group_id = aws_security_group.vpc_link.id
 }
 
 resource "aws_security_group_rule" "ecs_egress_rds" {
@@ -26,32 +26,6 @@ resource "aws_security_group_rule" "ecs_egress_rds" {
   source_security_group_id = aws_security_group.database.id
 }
 
-resource "aws_security_group_rule" "ecs_egress_mq" {
-  type                     = "egress"
-  from_port                = 5671
-  to_port                  = 5671
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.ecs_tasks.id
-  source_security_group_id = aws_security_group.mq.id
-}
-
-resource "aws_security_group_rule" "ecs_egress_mq_stomp" {
-  type                     = "egress"
-  from_port                = 61614
-  to_port                  = 61614
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.ecs_tasks.id
-  source_security_group_id = aws_security_group.mq.id
-}
-
-resource "aws_security_group_rule" "ecs_egress_redis" {
-  type                     = "egress"
-  from_port                = 6379
-  to_port                  = 6379
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.ecs_tasks.id
-  source_security_group_id = aws_security_group.redis.id
-}
 
 resource "aws_security_group_rule" "ecs_egress_https" {
   type              = "egress"
