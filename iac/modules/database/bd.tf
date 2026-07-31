221914029d8c56a7d043470a4ac682edaa782432
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-07-31 17:39:43 -0500

    feat: indicar single-az en bd

--- iac/modules/database/bd.tf
+++ iac/modules/database/bd.tf
@@ -3,6 +3,7 @@ resource "aws_db_instance" "postgres" {
   engine                 = "postgres"
   engine_version         = "17"
   instance_class         = "db.t3.micro"
+  multi_az               = false
   allocated_storage      = 20
 
   db_name                = "tesladb"
