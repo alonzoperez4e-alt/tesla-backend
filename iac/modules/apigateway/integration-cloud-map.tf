5026f34bd003559da706a479b129a92e6ddffe2e
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-07-31 18:33:54 -0500

    feat: integrar con cloud map

---
+++ iac/modules/apigateway/integration-cloud-map.tf
@@ -0,0 +1,9 @@
+resource "aws_apigatewayv2_integration" "backend" {
+  api_id = aws_apigatewayv2_api.main.id
+  integration_type = "HTTP PROXY"
+  integration_uri = var.service_discovery_arn
+  integration_method = "ANY"
+  connection_type = "VPC_LINK"
+  connection_id = aws_apigatewayv2_vpc_link.main.id
+  timeout_milliseconds = 15000
+}
